4fda6ec4bd4c0e1143886f550f32689927c1d6ad
demoner21 <a.tranhago@gmail.com>
2022-07-27 03:46:19 -0300

    storageFactory.sol

---
+++ Day02/Contract/storageFactory.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory is SimpleStorage {
+    
+    SimpleStorage[] public SSArray;
+
+    function createSSContract() public {
+        // generate a contract simpleStorage type
+        SimpleStorage generateSS = new SimpleStorage();
+        SSArray.push(generateSS);
+    }
+
+    function simpleFStore(uint256 index, uint256 age) public {
+        // get the contract that we wanna  intereact
+        SimpleStorage(address(SSArray[index])).store(age);
+    }
+
+    function simpleFRetrive(uint256 index) public view returns(uint256) {
+        // to acess our contract once again
+        return SimpleStorage(address(SSArray[index])).retrieve();
+    }
+}
