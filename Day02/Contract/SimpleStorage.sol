dbbfc8a155ba620cb7b4cada3d8ec284ec74f0dc
demoner21 <a.tranhago@gmail.com>
2022-07-27 03:45:57 -0300

    Main Contract to import and construct the base

---
+++ Day02/Contract/SimpleStorage.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+
+contract SimpleStorage {
+    uint256 age;
+
+    struct Pilot {
+        uint256 age;
+        string name;
+    }
+    Pilot[] public titan;
+
+    mapping(string => uint256) public pilotAge;
+
+    function store(uint256 _age) public virtual {
+        age = _age;
+    }
+
+    function retrieve() public view returns (uint256){
+        return age;
+    }
+
+    function add(string memory _name, uint256 _age) public {
+        titan.push(Pilot(_age, _name));
+        pilotAge[_name] = _age;
+    }
+}
