a27118f91f0318596f70bfb62d4f81f8fc4065f6
demoner21 <a.tranhago@gmail.com>
2022-07-27 03:45:08 -0300

    ExtraStorage override function

---
+++ Day02/Contract/ExtraStorage.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+
+import "./SimpleStorage.sol";
+
+contract ExtraStorage is SimpleStorage {
+
+    function store(uint256 x) public override {
+        age = x + 1;
+    }
+
+}
