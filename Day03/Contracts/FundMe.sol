bc3b90d98003d8005e92139e3ac8227973f3dbba
demoner21 <a.tranhago@gmail.com>
2022-07-28 15:55:01 -0300

    contract to test in the rinkeby network, with the address from datafeed chainlink ETH/USD

--- Day03/Contracts/FundMe.sol
+++ Day03/Contracts/FundMe.sol
@@ -1,13 +1,28 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.0 <0.90;
+pragma solidity >=0.4.2 <0.9.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
 
-    mapping(address => uint256) public addressFundedAmount;
+    uint256 public minimumUsd = 13;
 
     function fund() public payable {
-        addressFundedAmount[msg.sender] += msg.value;
+        // want to be able to set a minimun fund amount in usd
+        require(msg.value >= minimumUsd, "Didn't send enough, please check the balance");
     }
+
+    function getPrice() public {
+        // ABI
+        // Address 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+    }
+
+    function getVersion() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
+    }
+
+    // function withDraw(){}
 
 }
