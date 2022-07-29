c3fca5d1046839b3048dd59d0b111970f3e4a108
demoner21 <a.tranhago@gmail.com>
2022-07-29 00:48:33 -0300

    Finish the contract with address that send ETH, get current price from, conversion

--- Day03/Contracts/FundMe.sol
+++ Day03/Contracts/FundMe.sol
@@ -6,21 +6,36 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
 
-    uint256 public minimumUsd = 13;
+    uint256 public minimumUsd = 57 * 1e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAMountFunded;
 
     function fund() public payable {
         // want to be able to set a minimun fund amount in usd
-        require(msg.value >= minimumUsd, "Didn't send enough, please check the balance");
+        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough, please check the balance");
+        funders.push(msg.sender);
+        addressToAMountFunded[msg.sender] = msg.value;
     }
 
-    function getPrice() public {
+    function getPrice() public view returns(uint256){
         // ABI
         // Address 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+       (,int256 price,,,)= priceFeed.latestRoundData();
+        // Eth in terms of USD
+        // 3000.00000000
+       return uint256(price * 1e10); // 1**10 == 10000000000
     }
 
     function getVersion() public view returns(uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         return priceFeed.version();
+    }
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUSD;
     }
 
     // function withDraw(){}
