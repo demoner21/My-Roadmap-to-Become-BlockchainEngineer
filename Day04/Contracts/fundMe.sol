e4ac6df7eb3378fcb1851ed55594e9c6a9638565
demoner21 <a.tranhago@gmail.com>
2022-08-05 03:46:13 -0300

    finish day04, advanced Solidity concepts

--- Day04/Contracts/fundMe.sol
+++ Day04/Contracts/fundMe.sol
@@ -1,16 +1,25 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.4.2 <0.9.0;
+pragma solidity ^0.8.0;
 
 import "./priceConverter.sol";
+
+//error nowOwner();
 
 contract FundMe {
     using priceConverter for uint256;
 
-    uint256 public minimumUsd = 57 * 1e18;
+    uint256 public constant minimumUsd = 57 * 1e18;
 
     address[] public funders;
     mapping(address => uint256) public addressToAMountFunded;
+
+    address public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender; // sender of a msg is us
+                            //one that deploys the smartContract
+    }
 
     function fund() public payable {
         // want to be able to set a minimun fund amount in usd
@@ -19,6 +28,23 @@ contract FundMe {
         addressToAMountFunded[msg.sender] = msg.value;
     }
 
-    function withdraw() public {}
+    function withdraw() public onlyAdmin {
+        /* Start index, endng index, step amount */
+        for(uint256 fundersIndex = 0; fundersIndex < funders.length; fundersIndex++) {
+        //code
+            address funder = funders[fundersIndex];
+            addressToAMountFunded[funder] = 0;
+        }
+        // reset the array
+        funders = new address[](0);
+        (bool callSucess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSucess, "Call failed");
+    }
+
+    modifier onlyAdmin {
+        require(msg.sender != i_owner, "Sender is not allowed, is not owner");
+        //if (msg.sender != i_owner) { revert nowOwner(); }
+        _;
+    }
 
 }
